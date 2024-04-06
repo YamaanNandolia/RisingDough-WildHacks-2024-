39e831163b04971f1945b4fc239ff39c1ac98501
notcoose <markusgperez@gmail.com>
2024-04-06 16:23:11 -0500

    changed logo

--- nextjs-dashboard/app/ui/acme-logo.tsx
+++ nextjs-dashboard/app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { inter, robotoCondensed } from '@/app/ui/fonts';
+import { sansita } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${robotoCondensed.className} flex flex-row items-center leading-none text-white`}
+      className={`${sansita.className} flex flex-row items-center leading-none text-white`}
     >
       <p className="text-[44px]">Rising Dough</p>
     </div>
