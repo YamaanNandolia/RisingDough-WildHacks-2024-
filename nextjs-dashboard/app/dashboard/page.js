33d1e2de21dc9e1fa4b16997c1a1f01962e048a9
notcoose <markusgperez@gmail.com>
2024-04-07 10:03:14 -0500

    colors

--- nextjs-dashboard/app/dashboard/page.js
+++ nextjs-dashboard/app/dashboard/page.js
@@ -9,15 +9,15 @@ export default function Page() {
   return (
     <main className='grid grid-cols-1 md:grid-cols-2 gap-6'>
       <div className="md:col-span-1 gap-6 flex flex-col">
-        <div className="flex gap-6 rounded-lg bg-gray-50 px-10 py-5 md:px-20" >
+        <div className="flex gap-6 rounded-lg bg-gray-100 px-10 py-5 md:px-20" >
           <Doughnut />
         </div>
-        <div className="flex gap-6 rounded-lg bg-gray-50 px-10 py-5 md:px-20" >
+        <div className="flex gap-6 rounded-lg bg-gray-100 px-10 py-5 md:px-20" >
         <h1 className="text-2xl text-black-800 md:text-3xl text-bold text-center">Upload</h1>
           <UploadForm />
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center rounded-lg bg-gray-50 px-10 py-10 md:w-4/5 md:px-20">
+      <div className="flex flex-col items-center justify-center rounded-lg bg-gray-100 px-10 py-10 md:w-4/5 md:px-20">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-2xl text-black-800 md:text-3xl text-bold text-center">Connected Accounts</h1>
           <ListGroup className=" list my--3" flush>
